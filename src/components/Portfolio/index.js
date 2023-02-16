4bc625f254eda5a051c0c94e3121cee303104f69
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-15 21:49:28 -0500

    added pages folder and added projects page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,16 @@
-import About from "../About";
-import Contact from "../Contact";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 import Footer from "../Footer";
 import Main from "../Main";
 import Header from "../Header";
+
+
+const Portfolio = () => {
+    return(
+        <>
+        <Header/>
+        <Main/>
+        <Footer/>
+        </>
+    )
+};
---
+++ src/components/pages/Projects/index.js
@@ -0,0 +1,20 @@
+import Footer from "../../Footer"
+import Header from "../../Header"
+
+const Projects = () => {
+    return(
+        <>
+        <Header/>
+            <main>
+            <h3>Portfolio</h3>
+            <img src="https://via.placeholder.com/200" alt="placeholder image"/>    
+            <img src="https://via.placeholder.com/200" alt="placeholder image"/>    
+            <img src="https://via.placeholder.com/200" alt="placeholder image"/>    
+            <img src="https://via.placeholder.com/200" alt="placeholder image"/>    
+            </main>
+        <Footer/>
+        </>
+    )
+}
+
+export default Projects;
