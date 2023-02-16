f9dbb307208b2dba94c03b2efe62a14f16782edc
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-16 00:39:05 -0500

    attempted to add profile picture

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 const About = () => {
     return(
         <div>
-            <img src="https://via.placeholder.com/200" alt="placeholder image"/> 
+            <img src="https://via.placeholder.com/200" alt=" Terrance Smith"/> 
             <section>
                 <p>
                     this is paragraphy about me
